86858da177658bf34ff6dc1167c5bf8a9185381e
GabiBia <43347918+GabiBia@users.noreply.github.com>
2021-12-12 22:34:13 +0100

    Fixed comments

--- Rutherford.py
+++ Rutherford.py
@@ -9,18 +9,18 @@ def particle_definition(charge,radius):
     return sphere(pos=vector(0,0,0),color=color.yellow,charge=charge,radius=radius)
 
 def simulation_run(Gold):
-    while (len(Alphas)<500): # animuj, dopóki nie wystrzelimy ustalonej liczby cząstek alfa
+    while (len(Alphas)<500): # animacja do momentu wystrzelenia ustalonej liczby cząstek alfa
         rate(1/dt) # szybkość animacji
 
-        r = random() # losowo wybierz, kiedy wysłać cząsteczkę alfa
+        r = random() # losowy moment wystrzelenia cząstki alfa
         if (r < 0.1): # 10% szansa na strzał
             x = -1.0
             y = 2*random() - 1
             z = 2*random() - 1
-            # Utwórz cząsteczkę alfa i dodaj ją do listy.
+            # Tworzenie cząsteczki alfa i dodawanie jej do listy.
             Alphas.append(simple_sphere(pos=vector(x,y,z),velocity=vector(0.1,0,0),charge=2,mass=4,radius=0.01,color=color.red,make_trail=True))
 
-        for a in Alphas: # Literuj po cząstkach Alfs
+        for a in Alphas: # Iterowanie po cząstkach Alfa
             a.force = k*a.charge*Gold.charge/mag(a.pos)**2 * hat(a.pos)
             a.velocity += a.force/a.mass*dt
             a.pos += a.velocity*dt
